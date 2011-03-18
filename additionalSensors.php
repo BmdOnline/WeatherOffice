b5f459b7f8234169b34aee46c164aa8d173b8b8e
zucki <zucki@b02872bf-0d2e-0410-9e48-fbd7bcd034bf>
2011-03-18 21:52:26 +0000

    abfrage ob tabelle für sensoren existiert

--- additionalSensors.php
+++ additionalSensors.php
@@ -74,82 +74,94 @@ function GetCurrentValue($filename, $linenumber)
 	return $value;
 }
 
-$addSensor = $_REQUEST["addSensor"];
 
-if($addSensor == "false")
+$table = "additionalsensors";
+
+if( mysql_num_rows( mysql_query("SHOW TABLES LIKE '".$table."'")))
 {
-    // Sensorentabelle
-	echo "<hr><table border=\"1\"><tr>";
-	echo "<th>ID</th>";
-	echo "<th>Name</th>";
-	echo "<th>Datei</th>";
-	echo "<th>Zeilennummer</th>";
-	echo "<th>Aktueller Wert</th>";
-	echo "<th>Einheit</th></tr>";
 
-		
-	$result = SqlQuery("select * from additionalsensors ORDER BY id", false);
-	$cnt=mysql_num_rows($result);
-	for($i=0; $i<$cnt; $i++)
+	$addSensor = $_REQUEST["addSensor"];
+
+	if($addSensor == "false")
 	{
-		$id=mysql_result($result, $i, 'id');
-		$name=mysql_result($result, $i, 'name');
-		$filename=mysql_result($result, $i, 'filename');
-		$linenumber=mysql_result($result, $i, 'linenumber');
-		$unit=mysql_result($result, $i, 'unit');
-		$value=GetCurrentValue($filename, $linenumber);
-	
-		echo "<tr>";
-		echo "<td>$id</td>";
-		echo "<td>$name</td>";
-		echo "<td>$filename</td>";
-		echo "<td>$linenumber</td>";
-		echo "<td>$value</td>";
-		echo "<td>$unit</td>";
-		echo "</tr>";
+
+		// Sensorentabelle
+		echo "<hr><table border=\"1\"><tr>";
+		echo "<th>ID</th>";
+		echo "<th>Name</th>";
+		echo "<th>Datei</th>";
+		echo "<th>Zeilennummer</th>";
+		echo "<th>Aktueller Wert</th>";
+		echo "<th>Einheit</th></tr>";
+
+			
+		$result = SqlQuery("select * from $table ORDER BY id", false);
+		$cnt=mysql_num_rows($result);
+		for($i=0; $i<$cnt; $i++)
+		{
+			$id=mysql_result($result, $i, 'id');
+			$name=mysql_result($result, $i, 'name');
+			$filename=mysql_result($result, $i, 'filename');
+			$linenumber=mysql_result($result, $i, 'linenumber');
+			$unit=mysql_result($result, $i, 'unit');
+			$value=GetCurrentValue($filename, $linenumber);
+		
+			echo "<tr>";
+			echo "<td>$id</td>";
+			echo "<td>$name</td>";
+			echo "<td>$filename</td>";
+			echo "<td>$linenumber</td>";
+			echo "<td>$value</td>";
+			echo "<td>$unit</td>";
+			echo "</tr>";
+		}
+		mysql_free_result($result);
+		
+		echo "</table>";
+		
+		
+		// New Sensor
+		echo "<p><hr><b>Neuer Sensor:</b></p>";
+		echo "<form action = \"additionalSensors.php\" method=\"post\" target=\"main\">";
+			
+		echo "Name: ";
+		printf( "<input type = \"text\" size=\"15\" maxlenght=\"30\" name=\"name\" value=\"\">");
+
+		echo "   Dateiname: ";
+		printf( "<input type = \"text\" size=\"20\" maxlenght=\"255\" name=\"filename\" value=\"\">");
+			
+		echo "   Zeilennummer: ";
+		printf( "<input type = \"text\" size=\"3\" name=\"linenumber\" value=\"\">");
+
+		echo "   Einheit: ";
+		printf( "<input type = \"text\" size=\"7\"  maxlenght=\"20\" name=\"unit\" value=\"\">");
+
+		echo "<input type = \"submit\" value = \"Sensor hinzufügen\">";
+		
+		echo "<input type = \"hidden\" name=\"addSensor\" value=\"true\">";
+		echo "</form>";
 	}
-	mysql_free_result($result);
-	
-	echo "</table>";
-	
-	
-	// New Sensor
- 	echo "<p><hr><b>Neuer Sensor:</b></p>";
-	echo "<form action = \"additionalSensors.php\" method=\"post\" target=\"main\">";
+	else
+	{
+		$name = $_REQUEST["name"];
+		$filename = $_REQUEST["filename"];
+		$linenumber = $_REQUEST["linenumber"];
+		$unit = $_REQUEST["unit"];
+
+		$newSensorId = GetNextSensorID();
 		
-	echo "Name: ";
-	printf( "<input type = \"text\" size=\"15\" maxlenght=\"30\" name=\"name\" value=\"\">");
+		SqlQuery("INSERT INTO $table Values($newSensorId,
+							\"$name\", 				
+							\"$filename\", 
+							\"$linenumber\",
+							\"$unit\");", false);
 
-	echo "   Dateiname: ";
-	printf( "<input type = \"text\" size=\"20\" maxlenght=\"255\" name=\"filename\" value=\"\">");
-		
-	echo "   Zeilennummer: ";
-	printf( "<input type = \"text\" size=\"3\" name=\"linenumber\" value=\"\">");
-
-	echo "   Einheit: ";
-	printf( "<input type = \"text\" size=\"7\"  maxlenght=\"20\" name=\"unit\" value=\"\">");
-
-	echo "<input type = \"submit\" value = \"Sensor hinzufügen\">";
-	
-	echo "<input type = \"hidden\" name=\"addSensor\" value=\"true\">";
-	echo "</form>";
+		echo "Sensor \"$name\" hinzugefügt.<br>";
+	}
 }
 else
 {
-	$name = $_REQUEST["name"];
-	$filename = $_REQUEST["filename"];
-	$linenumber = $_REQUEST["linenumber"];
-	$unit = $_REQUEST["unit"];
-
-	$newSensorId = GetNextSensorID();
-	
-	SqlQuery("INSERT INTO additionalsensors Values($newSensorId,
-						\"$name\", 				
-						\"$filename\", 
-						\"$linenumber\",
-						\"$unit\");", false);
-
-	echo "Sensor \"$name\" hinzugefügt.<br>";
+	echo "mysql-Tabelle <b>\"$table\"</b> existiert nicht.<br>";
 }
 
 mysql_close();
